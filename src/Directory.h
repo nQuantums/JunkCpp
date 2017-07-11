f9b9d5d3c1dc840b3c4c93420785209afc0409a0
nQuantums <nquantums@gmail.com>
2017-07-12 02:20:05 +0900

    cppとその他ソース登録。

---
+++ src/Directory.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#ifndef __JUNK_DIRECTORY_H__
+#define __JUNK_DIRECTORY_H__
+
+#include "JunkConfig.h"
+#include <vector>
+#include <string>
+
+_JUNK_BEGIN
+
+//! ディレクトリ
+class Directory {
+public:
+	static ibool GetCurrent(std::string& curDir); //!< カレントディレクトリを取得する
+};
+
+_JUNK_END
+
+#endif
